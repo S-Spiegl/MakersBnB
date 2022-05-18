e3149671133dcdcf9006c221573bf179c16c0b8b
Tom O'Neill <tomoneill32@gmail.com>
2022-05-18 16:21:30 +0100

    Typo in migrations

--- db/migrations/01_create_database.sql
+++ db/migrations/01_create_database.sql
@@ -1,10 +1,10 @@
 CREATE DATABASE makersbnb;
 \c makersbnb;
 
-CREATE TABLE user(id SERIAL PRIMARY KEY, username VARCHAR(30));
+CREATE TABLE users (id SERIAL PRIMARY KEY, username VARCHAR(30));
 
 CREATE DATABASE makersbnb_test;
 \c makersbnb_test;
 
-CREATE TABLE user ( id SERIAL PRIMARY KEY,  username VARCHAR(30));
+CREATE TABLE users ( id SERIAL PRIMARY KEY,  username VARCHAR(30));
 
