6400e4ead0680eca98b11c2ce30ab540d6c04cf5
Jimmy Lyons <jimmylyons1@hotmail.co.uk>
2022-05-19 16:52:18 +0100

    Refactored database setup into object

--- lib/space.rb
+++ lib/space.rb
@@ -12,26 +12,16 @@ class Space
   end
 
   def self.all
-    if ENV['RACK_ENV'] == 'test'
-      connection = PG.connect(dbname: 'makersbnb_test')
-    else
-      connection = PG.connect(dbname: 'makersbnb')
-    end
 
-    result = connection.exec('SELECT * FROM spaces;')
+    result = DatabaseConnection.query('SELECT * FROM spaces;')
     result.map do |space|
       Space.new(id: space['id'], name_of_space: space['name_of_space'], available: space['available'])
     end
   end
 
   def self.create(name_of_space:, available: 1, user_id: 1 )
-    if ENV['RACK_ENV'] == 'test'
-      connection = PG.connect(dbname: 'makersbnb_test')
-    else
-      connection = PG.connect(dbname: 'makersbnb')
-    end
 
-    result = connection.exec_params(
+    result = DatabaseConnection.query(
       "INSERT INTO spaces (name_of_space, available, user_id) VALUES($1, $2, $3) RETURNING id, name_of_space, user_id;", 
       [name_of_space, available, user_id]
     )
@@ -39,26 +29,16 @@ class Space
   end
   
   def self.find_by_user(id:)
-    if ENV['RACK_ENV'] == 'test'
-      connection = PG.connect(dbname: 'makersbnb_test')
-    else
-      connection = PG.connect(dbname: 'makersbnb')
-    end
 
-    result = connection.exec_params("SELECT * FROM spaces WHERE user_id = ($1);",[id])
+    result = DatabaseConnection.query("SELECT * FROM spaces WHERE user_id = ($1);",[id])
     result.map do |space|
       Space.new(id: space['id'], name_of_space: space['name_of_space'], available: space['available'])
     end
   end 
 
   def self.find(id:)
-    if ENV['RACK_ENV'] == 'test'
-      connection = PG.connect(dbname: 'makersbnb_test')
-    else
-      connection = PG.connect(dbname: 'makersbnb')
-    end
   
-    space = connection.exec_params("SELECT * FROM spaces WHERE id = ($1);",[id])
+    space = DatabaseConnection.query("SELECT * FROM spaces WHERE id = ($1);",[id])
     Space.new(id: space[0]['id'], name_of_space: space[0]['name_of_space'], available: space[0]['available'])
   end 
   
